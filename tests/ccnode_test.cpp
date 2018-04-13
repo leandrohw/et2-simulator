c5650ce1170b3868a267abca5fbf5410f92a396a
Leandro Watanabe <16313257+leandrohw@users.noreply.github.com>
2018-04-12 17:09:46 -0700

    A couple tests for ccnode

--- tests/ccnode_test.cpp
+++ tests/ccnode_test.cpp
@@ -1,6 +1,22 @@
 #include "simulator/ccnode.h"
 #include "gtest/gtest.h"
 
+TEST(DefaultTest, Constructor) {
+  CCNode * node = new CCNode(1, 2, 3, 0);
+  ASSERT_EQ(node->getMethodId(), 1);
+  ASSERT_EQ(node->getThreadId(), 2);
+  ASSERT_EQ(node->getFirstCall(), 3);
+  ASSERT_EQ(node->getLastCall(), 3);
+  ASSERT_EQ(node->getParent(), nullptr);
+  ASSERT_EQ(node->getAllocBytes(), 0);
+  ASSERT_EQ(node->getAllocObjects(), 0);
+  ASSERT_EQ(node->getDeadBytes(), 0);
+  ASSERT_EQ(node->getDeadObjects(), 0);
+  ASSERT_EQ(node->getTotalAllocBytes(), 0);
+  ASSERT_EQ(node->getTotalDeadBytes(), 0);
+  ASSERT_EQ(node->getAllocRank(), 0);
+}
+
 TEST(DemandTest, DemandingNewChild) {
   CCNode * node = new CCNode(0, 0, 0, 0);
   CCNode * child = node->demand_child(1, 0, 0);
@@ -9,7 +25,7 @@ TEST(DemandTest, DemandingNewChild) {
 
 }
 
-TEST(ChildrenTest, DemandingOldChild) {
+TEST(DemandTest, DemandingOldChild) {
   CCNode * node = new CCNode(0, 0, 0, 0);
   CCNode * old_child = node->demand_child(1, 0, 0);
   CCNode * new_child = node->demand_child(1, 0, 0);
@@ -18,11 +34,37 @@ TEST(ChildrenTest, DemandingOldChild) {
 
 }
 
-TEST(ChildrenTest, DemandingMultipleChildren) {
+TEST(DemandTest, DemandingMultipleChildren) {
   CCNode * node = new CCNode(0, 0, 0, 0);
   CCNode * old_child = node->demand_child(1, 0, 0);
   CCNode * new_child = node->demand_child(2, 0, 0);
   ASSERT_EQ(node->getChildren().size(), 2);
   ASSERT_NE(old_child, new_child);
+
+}
+
+TEST(DemandTest, DemandingChildTime) {
+  CCNode * node = new CCNode(0, 0, 0, 0);
+  CCNode * child = node->demand_child(1, 0, 0);
+  ASSERT_EQ(child->getFirstCall(), 0);
+  ASSERT_EQ(child->getLastCall(), 0);
+
+  child = node->demand_child(1, 0, 10);
+  ASSERT_EQ(child->getFirstCall(), 0);
+  ASSERT_EQ(child->getLastCall(), 10);
+
+}
+
+
+TEST(DemandComputeTotal, DemandingChildTime) {
+  CCNode * node = new CCNode(0, 0, 0, 0);
+  node->incAllocBytes(32);
+
+  CCNode * child = node->demand_child(1, 0, 0);
+  child->incAllocBytes(16);
+
+  node->computeTotals();
+  ASSERT_EQ(node->getAllocBytes(), 32);
+  ASSERT_EQ(node->getTotalAllocBytes(), 48);
 
 }
